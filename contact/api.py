3d8cef54986e1fbc5d1672f3c37d93632085c7e4
Lucas Mgana <49315295+lucasmgana@users.noreply.github.com>
2022-03-30 06:29:02 -0700

    Update api.py

--- contact/api.py
+++ contact/api.py
@@ -4,6 +4,7 @@ from .serializers import ContactSerializer
 from .models import Contact
 
 
+# serializer class for contacts
 class ContactViewSet(viewsets.ModelViewSet):
     serializer_class = ContactSerializer
     queryset = Contact.objects.all()
